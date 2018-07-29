ac607da907832259aac267e4b8accca13398251f
Bipin Vekariya <bipinvekariya442@hotmail.com>
2018-07-29 20:43:00 +0530

    layout fixed with menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  showFiller = false;
+  opened = true;
+  mobileIcon = false;
+  mobileWindowSize = 599;
   userLanguage = '';
+  searchInput = '';
+  resetSearch = false;
+  menuExpand = false;
+  constructor(private router: Router) {
+
+  }
+  mobileMenu(): void {
+    this.opened = !this.opened;
+    this.mobileIcon = this.opened;
+  }
+
+  searchFocus(): void {
+    this.resetSearch = true;
+  }
+
+  searchFocusOut(): void {
+    if (!this.searchInput || this.searchInput.length === 0) {
+      this.resetSearch = false;
+    }
+
+  }
+
+  searchReset() {
+    this.resetSearch = false;
+  }
+
+  layoutClick() {
+    this.router.navigate(['/pagenotfound']);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < this.mobileWindowSize) {
+      this.opened = false;
+      this.mobileIcon = false;
+    } else {
+      this.opened = true;
+      this.mobileIcon = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatSidenavModule } from '@angular/material';
+import {
+  MatButtonModule, MatIconModule, MatSidenavModule, MatExpansionModule, MatMenuModule, MatDividerModule,
+  MatListModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatLineModule
+} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
@@ -10,19 +20,33 @@ import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.compon
 import { ErrorComponent } from './shared/error/error.component';
 import { RelPreloader } from './routing/rel-preloader';
 import { AuthenticationGuard } from './routing/authentication.guard';
+import { CommonModule } from '../../node_modules/@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     PagenotfoundComponent,
-    ErrorComponent 
+    ErrorComponent
   ],
   imports: [
+    MatLineModule,
     BrowserModule,
     MatButtonModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
-    MatIconModule,
+    CommonModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatMenuModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
     ToastrModule.forRoot({
       timeOut: 5000,
       positionClass: 'toast-bottom-right',
